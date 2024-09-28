8216018d38ae213dcc8beb221f5cd2affdc662dc
Ricarlos Junior <ricarlosjr@hotmail.com>
2024-09-27 21:14:27 -0300

    Atualizando os recursos, definindo retorno

--- payment-webhook-notify/src/main/java/br/com/link/payment/controller/NotificationController.java
+++ payment-webhook-notify/src/main/java/br/com/link/payment/controller/NotificationController.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import br.com.link.payment.dto.TransactionCompletionNotificationJSON;
 import br.com.link.payment.dto.TransactionCompletionNotificationPOST;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -19,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class NotificationController {
 
 	@PostMapping("/transaction/post")
-	public void transactionCompletionPOST(@RequestBody TransactionCompletionNotificationPOST dto) {
+	public ResponseEntity<TransactionCompletionNotificationPOST> transactionCompletionPOST(@RequestBody TransactionCompletionNotificationPOST dto) {
         try {
         	System.out.println("Chamou recurso: transactionCompletionPOST ");
         	System.out.println("**************************************** ");
@@ -27,13 +28,15 @@ public class NotificationController {
         	ObjectMapper objectMapper = new ObjectMapper();
 			String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(dto);
 			System.out.println(json);
+			
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
+        return ResponseEntity.ok().build();
 	}
 	
 	@PostMapping("/transaction/json")
-	public void transactionCompletionJSON(@RequestBody TransactionCompletionNotificationJSON dto) {
+	public ResponseEntity<TransactionCompletionNotificationJSON> transactionCompletionJSON(@RequestBody TransactionCompletionNotificationJSON dto) {
 		try {
 			System.out.println("Chamou recurso: transactionCompletionJSON ");
         	System.out.println("**************************************** ");
@@ -41,13 +44,15 @@ public class NotificationController {
             ObjectMapper objectMapper = new ObjectMapper();
 			String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(dto);
 			System.out.println(json);
+			
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
+		return ResponseEntity.ok().build();
 	}
 	
 	@PostMapping("/status/change")
-	public void statusChange(@RequestBody TransactionCompletionNotificationJSON dto) {
+	public ResponseEntity<TransactionCompletionNotificationJSON> statusChange(@RequestBody TransactionCompletionNotificationJSON dto) {
 		try {
 			System.out.println("Chamou recurso: statusChange ");
         	System.out.println("**************************************** ");
@@ -55,8 +60,11 @@ public class NotificationController {
             ObjectMapper objectMapper = new ObjectMapper();
 			String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(dto);
 			System.out.println(json);
+			
+			
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
+		return ResponseEntity.ok().build();
 	}
 }
